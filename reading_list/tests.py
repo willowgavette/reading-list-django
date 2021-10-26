d54da30d09a7436e3d557774462f56ccf23d6056
Willow Gavette <zoey.m.gavette@gmail.com>
2021-10-26 14:06:57 -0700

    added tests

--- reading_list/tests.py
+++ reading_list/tests.py
@@ -1,6 +1,12 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+from .models import Book
 
-class SimpleTests(SimpleTestCase):
-	def test_home_page_status_code(self):
-		response = self.client.get('/')
-		self.assertEqual(response.status_code, 200)
+class BookModelTest(TestCase):
+	def setUp(self):
+		Book.objects.create(title='just a test')
+		Book.objects.create(author='yet another test')
+
+	def test_title_content(self):
+		book=Book.objects.get(id=1)
+		expected_object_name = f'{book.title}'
+		self.assertEqual(expected_object_name, 'just a test')
