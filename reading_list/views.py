a167532f3332b706b7628d11e9ceb14bfdb6b906
Willow Gavette <zoey.m.gavette@gmail.com>
2021-10-29 16:52:39 -0700

    added user perms, still working on unique user databases

--- reading_list/views.py
+++ reading_list/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+	LoginRequiredMixin,
+	UserPassesTestMixin
+)
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import (
 	CreateView, 
@@ -8,27 +12,39 @@ from django.urls import reverse_lazy
 
 from .models import Book
 
-class ReadingListView(ListView):
+class ReadingListView(LoginRequiredMixin, ListView):
 	model = Book
 	template_name = 'home.html'
 	context_object_name = 'all_books_list'
 
-class SingleBookView(DetailView):
+class SingleBookView(LoginRequiredMixin, DetailView):
 	model = Book
 	template_name = 'book.html'
 	context_object_name = 'book'
 
-class BookEnterView(CreateView):
+class BookEnterView(LoginRequiredMixin, CreateView):
 	model = Book
 	template_name = 'book_entry.html'
-	fields = ['title', 'author']
+	fields = ('title', 'author')
 
-class BookUpdateView(UpdateView):
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Book
 	template_name = 'book_edit.html'
-	fields = ['title', 'author']
+	fields = ('title', 'author')
 
-class BookDeleteView(DeleteView):
+	def test_func(self):
+		obj = self.get_object()
+		return obj.author == self.request.user
+
+class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	model = Book
 	template_name = 'book_delete.html'
 	success_url = reverse_lazy('home')
+
+	def test_func(self):
+		obj = self.get_object()
+		return obj.author == self.request.user
